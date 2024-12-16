670226b13f55084916c7e073275d064ba459a1ec
im-Ahmed <ahmedfarooqim3@gamil.com>
2024-12-16 18:20:58 +0500

    blank plate

--- src/code.java
+++ src/code.java
@@ -1,9 +1,6 @@
 public class code {
-    public code() {
-        System.out.println("Hello world");
-    }
     public static void main(String[] args) {
-        code obj = new code();
+
     }
 
 
