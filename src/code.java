ed18f42af8688b107aadd4e8bf137f13a3cffdfb
im-Ahmed <ahmedfarooqim3@gamil.com>
2024-12-16 17:59:24 +0500

    second commit

--- src/code.java
+++ src/code.java
@@ -1,5 +1,10 @@
 public class code {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public code() {
+        System.out.println("Hello world");
     }
+    public static void main(String[] args) {
+        code obj = new code();
+    }
+
+
 }
